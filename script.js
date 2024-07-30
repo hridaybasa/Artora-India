11feacb96342820d3332c6b20f21d3d8b2eab2c5
hridaybasa <hridaybasa461@gmail.com>
2024-07-30 14:31:21 +0530

    section 3 done

--- script.js
+++ script.js
@@ -58,6 +58,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  const artistsBtn = document.querySelector(
+    ".nav-links .link a[href='#artists']"
+  );
+  if (artistsBtn) {
+    artistsBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      scrollToSection(2);
+      currentSection = 2; // Update currentSection to section3
+      showContent();
+    });
+  }
+
   const firstMenuContent = document.querySelector(".menu-content");
   firstMenuContent.classList.add("active"); // Add "active" class to the first content
   const firstMenuOption = document.querySelector(".menu-option");
@@ -83,4 +95,42 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   });
+
+  let nextDom = document.getElementById("next");
+  let prevDom = document.getElementById("prev");
+  let carouselDom = document.querySelector(".carousel");
+  let listItemDom = document.querySelector(".carousel .list");
+  let thumbnailDom = document.querySelector(".carousel .thumbnail");
+
+  nextDom.onclick = function () {
+    showSlider("next");
+  };
+  prevDom.onclick = function () {
+    showSlider("prev");
+  };
+  let timeRunning = 1000;
+  let runTimeOut;
+  function showSlider(type) {
+    let itemSlider = document.querySelectorAll(".carousel .list .item");
+    let itemThumbnail = document.querySelectorAll(
+      ".carousel .thumbnail .tnitem"
+    );
+
+    if (type === "next") {
+      listItemDom.appendChild(itemSlider[0]);
+      thumbnailDom.appendChild(itemThumbnail[0]);
+      carouselDom.classList.add("next");
+    } else {
+      let positionLastItem = itemSlider.length - 1;
+      listItemDom.prepend(itemSlider[positionLastItem]);
+      thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+      carouselDom.classList.add("prev");
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+      carouselDom.classList.remove("next");
+      carouselDom.classList.remove("prev");
+    }, timeRunning);
+  }
 });
